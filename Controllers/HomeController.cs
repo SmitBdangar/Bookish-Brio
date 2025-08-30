70c194ae33892bf3903deb65e5e3a5442e4336f9
Smit Dangar <smityadav011@gmail.com>
2025-08-30 10:46:02 +0530

    Fix the bug where slide bing dublicating

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,8 +15,15 @@ public class HomeController : Controller
 
     public ActionResult Index()
     {
+        var movies = new List<Movie>
+        {
+            new Movie{Id=1, Title="Inception", Genre="Sci-Fi", Description="A mind-bending thriller", VideoUrl="https://www.youtube.com/embed/YoHD9XEInc0"},
+            new Movie{Id=2, Title="The Matrix", Genre="Sci-Fi", Description="Reality is a simulation", VideoUrl="https://www.youtube.com/embed/vKQi3bBA1y8"},
+            new Movie{Id=3, Title="Spirited Away", Genre="Animation", Description="A young girl's adventure", VideoUrl=string.Empty}
+        };
+
         ViewBag.Message = "Welcome to MovieNest 🎬";
-        return View();
+        return View(movies);
     }
     public IActionResult Privacy()
     {
