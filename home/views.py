5aeffd0e2bd2bc19fb0570fe5f9f17e04eeef350
Smit Dangar <smityadav011@gmail.com>
2025-09-11 12:43:38 +0530

    F.a v1

--- home/views.py
+++ home/views.py
@@ -3,8 +3,12 @@ from .models import Post
 from .forms import PostForm
 
 def index(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'home/index.html', {'posts': posts})
+    from django.core.paginator import Paginator
+    post_list = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(post_list, 50)  # 50 posts per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'home/index.html', {'page_obj': page_obj})
 
 def add_post(request):
     if request.method == "POST":
