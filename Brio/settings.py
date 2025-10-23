a46fbe95312475caa5e8029679172f845f20936f
Smit Dangar <smityadav011@gmail.com>
2025-10-23 10:36:00 +0530

    final

--- Brio/settings.py
+++ Brio/settings.py
@@ -80,20 +80,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "Brio.wsgi.application"
 
-# Database
-DATABASES = {}
-DATABASE_URL = os.environ.get("DATABASE_URL")
-if DATABASE_URL:
-    DATABASES['default'] = dj_database_url.config(
-        default=DATABASE_URL,
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
         conn_max_age=600,
         ssl_require=True
     )
-else:
-    DATABASES['default'] = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+}
+
+# else:
+#     DATABASES['default'] = {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
@@ -116,3 +117,5 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
